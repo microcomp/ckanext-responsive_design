440107c0c63fe3eadc655b155541b895c35ee778
gyeprefos <farkas48@uniba.sk>
2015-02-11 13:10:23 +0100

    11.2.2015
    -done

---
+++ ckanext-responsive_design/ckanext/responsive_design/homepage.py
@@ -0,0 +1,44 @@
+import urllib
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import uuid
+import datetime
+
+import ckan.model as model
+import ckan.logic as logic
+import ckan.lib.base as base
+import ckan.lib.helpers as h
+import ckan.lib.navl.dictization_functions as df
+import ckan.plugins as p
+from ckan.common import _, c
+import ckan.plugins.toolkit as toolkit
+import urllib2
+import logging
+import ckan.logic
+import __builtin__
+
+
+import json
+def recent_datasets():
+    most_recent = model.Session.query(model.Package) \
+                    .filter(model.Package.state == 'active' and model \
+                    .Package.type == 'dataset').order_by(model.Package.metadata_modified.desc()) \
+                    .limit(10).all()
+    result = []
+    for i in most_recent:
+    	#text = model.Session.query(model.PackageRevision).filter(model.PackageRevision.name == i.name).first().notes
+        notes = i.notes
+    	if len(i.notes) > 300:
+            notes = i.notes[:300]+'...'
+        formats = model.Session.query(model.Resource).filter()
+        id = i.id
+        response = json.load(urllib2.urlopen('http://192.168.21.27:5000/api/3/action/resource_search?query=url:'+i.id))['result']['results']
+
+        res = []
+        for j in response:
+        	res.append(j["format"])
+
+        result.append({'title':i.title, 'text': notes, 'url':i.name, 'resources': res })
+    return result
+
--- ckanext-responsive_design/ckanext/responsive_design/plugin.py
+++ ckanext-responsive_design/ckanext/responsive_design/plugin.py
@@ -1,12 +1,30 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckan.logic as logic
+import json
+import os
 
+import ckan.logic
+import ckan.model as model
+from ckan.common import _, c
+import logging
+import homepage
 
 class ResponsiveDesign(plugins.SingletonPlugin):
+
     '''An example theme plugin.
 
     '''
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes, inherit=True)
+    plugins.implements(plugins.ITemplateHelpers, inherit=False)
+
     def update_config(self, config):
-	toolkit.add_template_directory(config, 'templates')
-	toolkit.add_public_directory(config, 'public')
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
+    def update_config(self, config):
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
+
+    def get_helpers(self):
+        return {'recent_datasets': homepage.recent_datasets}
