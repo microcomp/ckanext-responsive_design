0ec6c7341f513758014cc97bb0327b8cc75c0763
gyeprefos <farkas48@uniba.sk>
2015-03-10 09:21:54 +0100

    filter_by

--- ckanext/responsive_design/homepage.py
+++ ckanext/responsive_design/homepage.py
@@ -15,35 +15,26 @@ from ckan.common import _, c
 import ckan.plugins.toolkit as toolkit
 import urllib2
 import logging
-import ckan.logic as logic
+import ckan.logic
 import __builtin__
+from ckan.model.package import Package
 
 
 import json
-
-def uv_url():
-    if __builtin__.uv_url == None or __builtin__.uv_url == "":
-        __builtin__.uv_url = 'http://www.unifiedviews.eu/'
-    return __builtin__.uv_url
-def uv_usage():
-    context = {'user' : c.user}
-    try:
-        logic.check_access('uv_usage', context)
-        return True
-    except toolkit.NotAuthorized, e:
-        return False
-    
 def xwiki():
     if __builtin__.xwiki_url == None or __builtin__.xwiki_url == "":
         __builtin__.xwiki_url = 'http://xwiki.org'
     return __builtin__.xwiki_url
 def recent_datasets():
-    most_recent = model.Session.query(model.Package) \
-                    .filter(model.Package.state == 'active' and model \
-                    .Package.type == 'dataset').order_by(model.Package.metadata_modified.desc()) \
+    most_recent = model.Session.query(Package) \
+                    .filter_by(private=False).filter_by(type='dataset').filter_by(state='active').order_by(Package.metadata_modified.desc()) \
                     .limit(10).all()
+                    #Package.state == 'active' and Package.type == 'dataset' and 
     result = []
+    #most_recent = [x for x in most_recent if x.private == False]
+    #most_recent = most_recent[:10]
     for i in most_recent:
+        logging.warning(i.private)
     	#text = model.Session.query(model.PackageRevision).filter(model.PackageRevision.name == i.name).first().notes
         notes = i.notes
     	if len(i.notes) > 300:
