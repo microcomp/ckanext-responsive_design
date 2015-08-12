f2419570f615e7dc8de9bc19c12d3f4f24374d01
János Farkas <farkas48@uniba.sk>
2015-08-12 12:09:31 +0200

    Update plugin.py

--- ckanext/responsive_design/plugin.py
+++ ckanext/responsive_design/plugin.py
@@ -8,18 +8,15 @@ import ckan.logic
 import ckan.model as model
 from ckan.common import _, c
 import logging
-import homepage
+import helpers
 import __builtin__
 
 class ResponsiveDesign(plugins.SingletonPlugin):
 
-    '''An example theme plugin.
-
-    '''
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IRoutes, inherit=True)
     plugins.implements(plugins.ITemplateHelpers, inherit=False)
-    plugins.implements(plugins.IConfigurable)
+
 
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
@@ -27,28 +24,10 @@ class ResponsiveDesign(plugins.SingletonPlugin):
 
 
     def get_helpers(self):
-        return {'recent_datasets': homepage.recent_datasets,
-                'uv_url' : homepage.uv_url,
-                'xwiki': homepage.xwiki,
-                'geomodul_url': homepage.geomodul_url,
-                'onto_editor': homepage.onto_editor,
-                'user_req_url':homepage.user_req_url}
-
-    def configure(self, config):
-        self.site_url = config.get('ckan.site_url', None)
-        __builtin__.site_url = self.site_url
-
-        self.onto_editor = config.get('ckan.onto_url', None)
-        __builtin__.onto_editor = self.onto_editor
-
-        self.xwiki_url = config.get('ckan.xwiki.url', None)
-        __builtin__.xwiki_url = self.xwiki_url
-
-        self.user_req_url = config.get('ckan.user_req.url', None)
-        __builtin__.user_req_url = self.user_req_url
-
-        self.uv_url = config.get('odn.uv.url', None)
-        __builtin__.uv_url = self.uv_url
-        self.geomodul_url = config.get('ckan.geomodul.url', None)
-        __builtin__.geomodul_url = self.geomodul_url
+        return {'recent_datasets': helpers.recent_datasets,
+                'uv_url' : helpers.uv_url,
+                'xwiki': helpers.xwiki,
+                'geomodul_url': helpers.geomodul_url,
+                'onto_editor': helpers.onto_editor,
+                'user_req_url':helpers.user_req_url}
 
