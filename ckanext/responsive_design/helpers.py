021960a8a1cbf074eb9c7302efca5c4d593e5522
János Farkas <farkas48@uniba.sk>
2015-08-12 12:10:39 +0200

    Update and rename homepage.py to helpers.py

--- ckanext/responsive_design/helpers.py
+++ ckanext/responsive_design/helpers.py
@@ -1,5 +1,5 @@
 import urllib
-
+ # -*- coding: utf-8 -*-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import uuid
@@ -18,31 +18,42 @@ import logging
 import ckan.logic
 import __builtin__
 from ckan.model.package import Package
-
+from pylons import config
 
 import json
 def uv_url():
-    if __builtin__.uv_url == None or __builtin__.uv_url == "":
-        __builtin__.uv_url = 'http://www.unifiedviews.eu/'
-    return __builtin__.uv_url
+    uv_url = config.get('odn.uv.url', None)
+    if uv_url == None or uv_url == "":
+        uv_url = 'http://www.unifiedviews.eu/'
+    return uv_url
+
 def user_req_url():
-    if __builtin__.user_req_url == None or __builtin__.user_req_url == "":
-        __builtin__.user_req_url = '#'
-    return __builtin__.user_req_url
+    user_req_url = config.get('ckan.user_req.url', None)
+    if user_req_url == None or user_req_url == "":
+        user_req_url = '#'
+    result = []
+    if user_req_url != '#':
+        for i in user_req_url.split(','):
+            result.append({'name':i.split('|')[0].decode('utf8'), 'url':i.split('|')[1], 'role':i.split('|')[2], 'popis':i.split('|')[3].decode('utf8')})
+    return result
     
 def onto_editor():
-    if __builtin__.onto_editor == None or __builtin__.onto_editor == "":
-        __builtin__.onto_editor = '#'
-    return __builtin__.onto_editor
+    onto_editor = config.get('ckan.onto_url', None)
+    if onto_editor == None or onto_editor == "":
+        onto_editor = '#'
+    return onto_editor
 
 def geomodul_url():
-    if __builtin__.geomodul_url == None or __builtin__.geomodul_url == "":
-        __builtin__.geomodul_url = '/dashboard'
-    return __builtin__.geomodul_url
+    geomodul_url = config.get('ckan.geomodul.url', None)
+    if geomodul_url == None or geomodul_url == "":
+        geomodul_url = '/dashboard'
+    return geomodul_url
 def xwiki():
-    if __builtin__.xwiki_url == None or __builtin__.xwiki_url == "":
-        __builtin__.xwiki_url = 'http://xwiki.org'
-    return __builtin__.xwiki_url
+    xwiki_url = config.get('ckan.xwiki.url', None)
+    if xwiki_url == None or xwiki_url == "":
+        xwiki_url = 'http://xwiki.org'
+    return xwiki_url
+
 def recent_datasets():
     context = {'model': model, 'session': model.Session,
                    'user': c.user or c.author,
