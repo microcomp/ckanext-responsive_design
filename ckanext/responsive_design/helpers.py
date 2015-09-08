bd26b988bd5a1b2f4e9b364985980a50d7f50178
Dominik Kapišinský <kapisinsky@microcomp.sk>
2015-09-08 14:52:33 +0200

    refactor

--- ckanext/responsive_design/helpers.py
+++ ckanext/responsive_design/helpers.py
@@ -1,26 +1,12 @@
-import urllib
- # -*- coding: utf-8 -*-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import uuid
-import datetime
+import logging
 
 import ckan.model as model
-import ckan.logic as logic
-import ckan.lib.base as base
-import ckan.lib.helpers as h
-import ckan.lib.navl.dictization_functions as df
-import ckan.plugins as p
 from ckan.common import _, c
 import ckan.plugins.toolkit as toolkit
-import urllib2
-import logging
-import ckan.logic
-import __builtin__
-from ckan.model.package import Package
-from pylons import config
+from pylons import config, session
 
-import json
+log = logging.getLogger(__name__)
+
 def uv_url():
     uv_url = config.get('odn.uv.url', None)
     if uv_url == None or uv_url == "":
@@ -36,22 +22,14 @@ def HR(uRoles, lRoles):
     return False
 def get_urls(text):
     user_roles = session.get('ckanext-cas-roles', [])
-    startTime = int(round(time.time() * 1000))
     _list =sorted([x for x in set( x.split('.')[2] for x in [x for x in config.keys() if text in x])])
     result = []
     for i in _list:
         result.append({'name':config.get(text+i+'.name').decode('utf8'), 
                        'url':config.get(text+i+'.url'), 
-                       'role':config.get(text+i+'.privileg').split(','), 
+                       'role':config.get(text+i+'.privilege').split(','), 
                        'popis':config.get(text+i+'.title').decode('utf8')})
 
-    endTime = int(round(time.time() * 1000))
-
-    logging.warning("menuitems "+text)
-    logging.warning(result)
-    t = str(endTime - startTime)+'ms'
-    logging.warning("Time: ")
-    logging.warning(t)
     result = [x for x in result if HR(user_roles, x['role'])]
     result2 = []
     for i in result:
@@ -103,6 +81,5 @@ def recent_datasets():
         for r in i['resources']:
             res.append(r['format'])
         result.append({'title':i['title'], 'text': notes, 'url':i['name'], 'resources': res })
-
     return result
 
